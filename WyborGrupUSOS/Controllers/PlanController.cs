fb4c0581d791098221b0ae842c3bfb2694678f88
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-05-25 20:03:48 +0200

    Prototyp pobierania linków do przedmiotów

---
+++ WyborGrupUSOS/Controllers/PlanController.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc;
+using WyborGrupUSOS.Models;
+
+namespace WyborGrupUSOS.Controllers
+{
+    public class PlanController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> LoadPlan(WebsiteLink model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            HttpClient hc = new HttpClient();
+            HttpResponseMessage result = await hc.GetAsync(model.Link);
+            Plan plan = new Plan { StatusCode = result.StatusCode.ToString()};
+
+            Stream stream = await result.Content.ReadAsStreamAsync();
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(stream);
+
+            HtmlNodeCollection subjectLinks = doc.DocumentNode.SelectNodes(@"//map").First().ChildNodes;
+            plan.Dane = subjectLinks;
+
+            return View("DisplayPlan", plan);
+        }
+    }
+}
---
+++ WyborGrupUSOS/Models/Plan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace WyborGrupUSOS.Models
+{
+    public class Plan
+    {
+        public string StatusCode { get; set; }
+        public HtmlNodeCollection Dane { get; set; }
+    }
+}
