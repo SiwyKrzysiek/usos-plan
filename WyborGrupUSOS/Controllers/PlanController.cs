8173c97354ea6513b1e76571b4595112a535a8de
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-05-31 19:04:24 +0200

    Wyodrębnienie komórek z opisem przedmiotów z html-a

--- WyborGrupUSOS/Controllers/PlanController.cs
+++ WyborGrupUSOS/Controllers/PlanController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web;
 using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
@@ -30,8 +31,13 @@ namespace WyborGrupUSOS.Controllers
                 return View("Index", model);
             }
 
+            var queryString = HttpUtility.ParseQueryString(model.Link);
+            queryString.Set("plan_format", "html");
+            var link = queryString.ToString();
+
             HttpClient httpClient = new HttpClient();
-            HttpResponseMessage result = await httpClient.GetAsync(model.Link);
+            HttpResponseMessage result = await httpClient.GetAsync(link);
+
             Plan plan = new Plan { StatusCode = result.StatusCode.ToString()};
 
             Stream stream = await result.Content.ReadAsStreamAsync();
@@ -39,22 +45,44 @@ namespace WyborGrupUSOS.Controllers
             HtmlDocument doc = new HtmlDocument();
             doc.Load(stream);
 
-            HtmlNodeCollection classesLinks = doc.DocumentNode.SelectNodes(@"//map").First().ChildNodes;
-            var classes = ExtractClasses(doc, httpClient);
+            ExtractClassesFromHtmlView(doc);
+            
+            var classes = ExtractClassesFromGifView(doc, httpClient);
 
+            HtmlNodeCollection classesLinks = doc.DocumentNode.SelectNodes(@"//map").First().ChildNodes;
             plan.Dane = classesLinks;
+
             plan.Classes = classes.ToList();
 
             return View("DisplayPlan", plan);
         }
 
+        private IEnumerable<UniversityClass> ExtractClassesFromHtmlView(HtmlDocument planPage)
+        {
+            var table = planPage.DocumentNode.SelectSingleNode(
+                @"//div[@id='layout-c22a']/div[@class='wrtext']/div[@class='wrtext']/div[@class='wrtext']/div/table[@cellspacing='0' and @cellpadding='0']"
+                );
+
+            var dataNodes = table.SelectNodes(@"//td").Where(IsClassData);
+
+            return null;
+        }
+
+        private bool IsClassData(HtmlNode td)
+        {
+            var count = td.ChildNodes.Count;
+            if (td.ChildNodes.Count != 2) return false;
+
+            return td.SelectSingleNode(@"span[@class='note']") != null;
+        }
+
         /// <summary>
-        /// Extract all classes from main view of Usos plan
+        /// Extract all classes from <b>gif</b> view of Usos plan
         /// </summary>
         /// <param name="planPage">Default semester plan page</param>
         /// <param name="httpClient">Client used to download data</param>
         /// <returns>List of classes</returns>
-        private IEnumerable<UniversityClass> ExtractClasses(HtmlDocument planPage, HttpClient httpClient)
+        private IEnumerable<UniversityClass> ExtractClassesFromGifView(HtmlDocument planPage, HttpClient httpClient)
         {
             var classesLinks = planPage.DocumentNode.SelectNodes(@"//map").First().ChildNodes;
             
