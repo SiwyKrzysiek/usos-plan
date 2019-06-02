a2494cbf38e53424ac562f158a38233dae8a03b4
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-06-02 10:36:58 +0200

    Zgrupowanie tych samych zajęć

--- WyborGrupUSOS/Models/Plan.cs
+++ WyborGrupUSOS/Models/Plan.cs
@@ -11,5 +11,29 @@ namespace WyborGrupUSOS.Models
         public string StatusCode { get; set; }
         public HtmlNodeCollection Dane { get; set; }
         public List<UniversityClass> Classes { get; set; }
+
+        public Dictionary<Tuple<string, UniversityClass.ClassType>, List<UniversityClass>> ClassGroups { get; set; }
+
+        public Plan()
+        {
+        }
+
+        public Plan(IEnumerable<UniversityClass> classes)
+        {
+            //TODO: Group classes with same type
+            foreach (var universityClass in classes)
+            {
+                var signature = universityClass.GetSignature();
+
+                if (!ClassGroups.ContainsKey(signature))
+                {
+                    ClassGroups.Add(signature, new List<UniversityClass>{universityClass});
+                }
+                else
+                {
+                    ClassGroups[signature].Add(universityClass);
+                }
+            }
+        }
     }
 }
