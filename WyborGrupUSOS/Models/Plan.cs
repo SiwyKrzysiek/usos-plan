dd8617f960e00ecb4b557e4dc1a5e1fae8dfe7a5
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-06-13 14:50:44 +0200

    Wydzielenie grup od pojedyńczych zajęć

--- WyborGrupUSOS/Models/Plan.cs
+++ WyborGrupUSOS/Models/Plan.cs
@@ -8,20 +8,34 @@ namespace WyborGrupUSOS.Models
 {
     public class Plan
     {
+        private List<UniversityClass> _classes;
         public string StatusCode { get; set; }
-        public HtmlNodeCollection Dane { get; set; }
-        public List<UniversityClass> Classes { get; set; }
-        public List<ClassGroup> ClassGroups { get; set; }
 
-        //public Dictionary<Tuple<string, UniversityClass.ClassType>, List<UniversityClass>> ClassGroupsDictionary { get; set; }
-
-        public Plan()
+        public List<UniversityClass> Classes
         {
+            get => _classes;
+            set => LoadClasses(value);
         }
+
+        /// <summary>
+        /// Classes that have only one group
+        /// </summary>
+        public List<UniversityClass> SingularClasses { get; private set; }
+        /// <summary>
+        /// Groups that have more than one member
+        /// </summary>
+        public List<ClassGroup> ClassGroups { get; private set; }
 
         public Plan(IEnumerable<UniversityClass> classes)
         {
-            Classes = classes.ToList();
+            SingularClasses = new List<UniversityClass>();
+            ClassGroups = new List<ClassGroup>();
+            LoadClasses(classes);
+        }
+
+        private Dictionary<Tuple<string, UniversityClass.ClassType>, List<UniversityClass>> CreateInitialGrouping(
+            IEnumerable<UniversityClass> classes)
+        {
             var classGroupsDictionary = new Dictionary<Tuple<string, UniversityClass.ClassType>, List<UniversityClass>>();
 
             foreach (var universityClass in Classes)
@@ -30,7 +44,7 @@ namespace WyborGrupUSOS.Models
 
                 if (!classGroupsDictionary.ContainsKey(signature))
                 {
-                    classGroupsDictionary.Add(signature, new List<UniversityClass>{universityClass});
+                    classGroupsDictionary.Add(signature, new List<UniversityClass> { universityClass });
                 }
                 else
                 {
@@ -38,7 +52,25 @@ namespace WyborGrupUSOS.Models
                 }
             }
 
-            ClassGroups = (from g in classGroupsDictionary select new ClassGroup(g.Key.Item1, g.Key.Item2, g.Value)).ToList();
+            return classGroupsDictionary;
+        }
+
+        private void LoadClasses(IEnumerable<UniversityClass> classes)
+        {
+            _classes = classes.ToList();
+            var classGroupsDictionary = CreateInitialGrouping(_classes);
+
+            foreach (var group in classGroupsDictionary)
+            {
+                if (group.Value.Count == 1)
+                    SingularClasses.Add(group.Value.First());
+                else
+                {
+                    var name = group.Value.First().Name;
+                    var type = group.Value.First().Type;
+                    ClassGroups.Add(new ClassGroup(name, type, group.Value));
+                }
+            }
         }
     }
 }
